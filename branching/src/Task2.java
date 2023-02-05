90bf811c178425f372cb0dc603b291e05622bfff
kinchi <nik.ch.2006@gmail.com>
2023-02-05 21:27:08 +0300

    0

--- branching/src/Task2.java
+++ branching/src/Task2.java
@@ -1,26 +1,65 @@
 
-
+import java.util.Scanner;
 
 public class Task2 {
     public static void main(String[] args) {
 
-        int cash = 159_890;
-        String company = "Bank";
-        int Tax;
+        double earnings = 159_890;
+        String BankCo = "Bank";
+        String TransportCo = "Transport";
+        String HealthCo = "Health";
+        double tax;
 
-        if (cash <= 250_000) {
+        Scanner Keyboard = new Scanner(System.in);
+        System.out.println("Выберите номер соответсвующий вашей компании");
+        System.out.println("1. bank \n2. Transport \n3. Health");
+        int companyType = Keyboard.nextInt();
 
-            Tax = (int) (cash * 0.1);
-            System.out.println("Налог на сумму " + cash + " для отрасли " + company + " составляет: " + Tax);
-            System.out.println("Это составляет 10%");
-        } else if (cash >= 250_000 && cash <= 499_999) {
-            Tax = (int) (cash * 0.15);
-            System.out.println("Налог на сумму " + cash + " для отрасли " + company + " составляет: " + Tax);
-            System.out.println("Это составляет 15%");
-        } else {
-            Tax = (int) (cash * 0.18);
-            System.out.println("Налог на сумму " + cash + " для отрасли " + company + " составляет: " + Tax);
-            System.out.println("Это составляет 18%");
+
+        if (companyType == 1) {
+
+            if (earnings <= 250_000) {
+                tax = (earnings * 0.1);
+                System.out.println("Налог на сумму " + earnings + " для отрасли " + BankCo + " составляет: " + tax);
+                System.out.println("Это составляет 10%");
+            } else if (earnings >= 250_000 && earnings <= 499_999) {
+                tax = (earnings * 0.15);
+                System.out.println("Налог на сумму " + earnings + " для отрасли " + BankCo + " составляет: " + tax);
+                System.out.println("Это составляет 15%");
+            } else {
+                tax = (earnings * 0.18);
+                System.out.println("Налог на сумму " + earnings + " для отрасли " + BankCo + " составляет: " + tax);
+                System.out.println("Это составляет 18%");
+            }
+        } else if (companyType == 2) {
+            if (earnings <= 100_000) {
+                tax = (earnings * 0.09);
+                System.out.println("Налог на сумму " + earnings + " для отрасли " + TransportCo + " составляет: " + tax);
+                System.out.println("Это составляет 9%");
+            } else if (earnings >= 100_000 && earnings <= 299_999) {
+                tax = (earnings * 0.13);
+                System.out.println("Налог на сумму " + earnings + " для отрасли " + TransportCo + " составляет: " + tax);
+                System.out.println("Это составляет 13%");
+            } else {
+                tax = (earnings * 0.17);
+                System.out.println("Налог на сумму " + earnings + " для отрасли " + TransportCo + " составляет: " + tax);
+                System.out.println("Это составляет 17%");
+            }
+        }
+        else {
+            if (earnings <= 250_000) {
+                tax = (earnings * 0.07);
+                System.out.println("Налог на сумму " + earnings + " для отрасли " + HealthCo + " составляет: " + tax);
+                System.out.println("Это составляет 7%");
+            } else if (earnings >= 250_000 && earnings <= 499_999) {
+                tax = (earnings * 0.12);
+                System.out.println("Налог на сумму " + earnings + " для отрасли " + HealthCo + " составляет: " + tax);
+                System.out.println("Это составляет 12%");
+            } else {
+                tax = (earnings * 0.15);
+                System.out.println("Налог на сумму " + earnings + " для отрасли " + HealthCo + " составляет: " + tax);
+                System.out.println("Это составляет 15%");
+            }
         }
 
     }
