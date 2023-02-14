407bcb0228d873291e6d39358e71268a47058f9a
kinchi <nik.ch.2006@gmail.com>
2023-02-14 14:36:13 +0300

    0

--- strings/src/Task1.java
+++ strings/src/Task1.java
@@ -1,16 +1,50 @@
 
-
+import java.util.Scanner;
 
 
 public class Task1 {
     public static void main(String[] args) {
 
-        String str = "Hello world";
-        String replStr1 = str.replace('l', 'd'); // Heddo wordd
-        String replStr2 = str.replace("Hello", "Bye"); // Bye world
+        Scanner keyboard = new Scanner(System.in);
 
-        System.out.println(replStr2);
+        int[] array = new int[5];
+        int temp = 0;
 
+        System.out.println("Введите 5 чисел");
 
+        for (int i = 0; i < 6; i++) {
+            switch (i) {
+                case (1) -> {
+                    System.out.println("Введите Первое число: ");
+                    array[0] = keyboard.nextInt();
+                }
+                case (2) -> {
+                    System.out.println("Введите Второе число: ");
+                    array[1] = keyboard.nextInt();
+                }
+                case (3) -> {
+                    System.out.println("Введите Третье число: ");
+                    array[2] = keyboard.nextInt();
+                }
+                case (4) -> {
+                    System.out.println("Введите Четвёртое число: ");
+                    array[3] = keyboard.nextInt();
+                }
+                case (5) -> {
+                    System.out.println("Введите Пятое число: ");
+                    array[4] = keyboard.nextInt();
+                }
+            }
+        }
+        for (int e : array){
+            if (e!=0){
+                temp = e;
+                temp = temp%2;
+                if (temp==0)
+                System.out.println("Число "+e+" чётное");
+            } if (temp!=0){
+                System.out.println("Число "+e+" не чётное");
+            }
+        }
     }
 }
