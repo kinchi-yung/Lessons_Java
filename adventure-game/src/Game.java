7e3d1a5daac68e9f9814dec39c24ed3c54378279
kinchi <nik.ch.2006@gmail.com>
2023-02-18 00:22:57 +0300

    0

--- adventure-game/src/Game.java
+++ adventure-game/src/Game.java
@@ -3,7 +3,11 @@
 
 public class Game {
     static boolean isGameFinished;
+
     public static void main(String[] args) {
+
+       // Rooms bedroom = new Rooms("","");
+
 
         System.out.println("Добро пожаловать в игру \"Late for work\", твоя цель - найти выход из дома");
 
--- adventure-game/src/Item.java
+++ adventure-game/src/Item.java
@@ -4,7 +4,7 @@ public class Item {
     private String name;
     private String description;
     private String roomName;
-    private String roomDesc;
+    private String roomDescription;
 
     static void Item() {
         System.out.println("У предмета должно быть своё название и описание!");
@@ -14,25 +14,61 @@ public class Item {
         this.name = name;
         this.description = description;
     }
-    void rooms() {
-        System.out.println("Дайте название комнате");
+    public void setName(String name){
+        if (name.isEmpty()){
+            System.out.println("У предмета должно быть хотя бы имя");
+        }else {
+            this.name=name;
+        }
+    }
+    public String getName(){
+        return name;
+    }
+    public void setDescription(String description){
+    }
+    public String getDescription(){
+        return description;
     }
 
-    void rooms(String roomname, String roomdescription) {
-        this.roomName = roomname;
-        this.roomDesc = roomdescription;
+
+
+
+
+
+//    void Rooms() {
+//        System.out.println("Дайте название комнате");
+//    }
+//
+//    void Rooms(String roomName, String roomDescription) {
+//        this.roomName = roomName;
+//        this.roomDescription = roomDescription;
+//    }
+//
+//    void Rooms(String name) {
+//        Rooms();
+//        roomDescription = "";
+//    }
+
+    public void setRoomName (String roomName){
+        if (roomName.isEmpty()){
+            System.out.println("У комнаты должно быть хотя бы имя");
+        }else {
+            this.roomName=roomName;
+        }
+    }
+    public String getRoomName(){
+        return roomName;
     }
 
-    void rooms(String name) {
-        rooms();
-        roomDesc = "";
+    public void setRoomDescription (String roomDescription){
+    }
+    public String getRoomDescription(){
+        return roomDescription;
     }
 
 }
 
-//public Item(String name, String description) { this.name = name; this.description = description; }
-//public Item(String name) { this(name, ""); }
-//public class Item { String name; String description;
+
 //    public Item(String name, String description) { this.name = name; this.description = description; }
 //
 //public Item(String name) { this(name, ""); } }
