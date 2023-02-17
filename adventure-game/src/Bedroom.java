065f9a2533de25e8812915fd1d48cbe904c2baa9
kinchi <nik.ch.2006@gmail.com>
2023-02-17 19:57:19 +0300

    0

--- adventure-game/src/Bedroom.java
+++ adventure-game/src/Bedroom.java
@@ -5,21 +5,32 @@ public class Bedroom {
     private String name = "Спальня";
     private String description = "";
 
-    Item[] items = new Item[7];
+    public static void main(String[] args) {
+        Item[] items = new Item[] {new Item ("Зеркало", "Простое зеркальце на прикроватной тумбе"),
+                new Item("Кровать", "На ней я сплю. Большая и любимая"),
+                new Item("Окно", "Совсем новое. Из него видно улицу с третьего этажа"),
+                new Item("Шкаф", "Шкаф в котором храню одежду"),
+                new Item("Дверь", "Выход и спальни"),
+                new Item("Стол", "За ним я ем, работаю, отдыхаю"),
+                new Item("Обычный смартфон", "Такой сейчас есть у каждого")};
+        for (Item s : items){
+            System.out.println(s);
+        }
+        System.out.println(items[1].toString());
+    }
+//    Item mirror = new Item("Зеркало", "Простое зеркальце на прикроватной тумбе");
+//    Item bed = new Item("Кровать", "На ней я сплю. Большая и любимая");
+//    Item window = new Item("Окно", "Совсем новое. Из него видно улицу с третьего этажа");
+//    Item closet = new Item("Шкаф", "Шкаф в котором храню одежду");
+//    Item door = new Item("Дверь", "Выход и спальни");
+//    Item table = new Item("Стол", "За ним я ем, работаю, отдыхаю");
+//    Item phone = new Item("Обычный смартфон", "Такой сейчас есть у каждого");
 
-    Item mirror = new Item("Зеркало", "Простое зеркальце на прикроватной тумбе");
-    Item bed = new Item("Кровать", "На ней я сплю. Большая и любимая");
-    Item window = new Item("Окно", "Совсем новое. Из него видно улицу с третьего этажа");
-    Item closet = new Item("Шкаф", "Шкаф в котором храню одежду");
-    Item door = new Item("Дверь", "Выход и спальни");
-    Item table = new Item("Стол", "За ним я ем, работаю, отдыхаю");
-    Item phone = new Item("Обычный смартфон", "Такой сейчас есть у каждого");
-
-    Item[0] = "mirror";
-
-    add(Item items[], mirror);
 
 
+//    items[0] = mirror;
+//
+//    add(Item items[], mirror);
 }
 //    To add an object to an existing array of objects in Java, you can use the Arrays class and its static method add(Object[] array, Object element). Here's an example:
 //
