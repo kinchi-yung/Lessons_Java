5f05c7987a9e5394eddec2944a09e096c3fec019
kinchi <nik.ch.2006@gmail.com>
2023-02-19 23:06:32 +0300

    0

--- adventure-game/src/Game.java
+++ adventure-game/src/Game.java
@@ -3,20 +3,16 @@ import java.util.Scanner;
 public class Game {
     static boolean isGameFinished;
 
+    Bedroom bedroom = new Bedroom("Спальня");
+    Kitchen kitchen = new Kitchen("Поедальня");
+    Bathroom bathroom = new Bathroom("Умывальня");
+    LivingRoom livingRoom = new LivingRoom("Гостевальня","Большная");
+
     public static void main(String[] args) {
         Scanner keyboard = new Scanner(System.in);
-
-        Item.Rooms Bathroom = new Item.Rooms("Умывальня");
-        Item.Rooms Bedroom = new Item.Rooms("Спальня");
-        Item.Rooms Kitchen = new Item.Rooms("Поедальня");
-        Item.Rooms LivingRoom = new Item.Rooms("Гостевальня", "Большная");
-
+        
         System.out.println("Добро пожаловать в игру \"Late for work\", твоя цель - найти выход из дома");
 
-        System.out.println(Bedroom.getRoomName());
-        System.out.println(Bathroom.getRoomName());
-        System.out.println(Kitchen.getRoomName());
-        System.out.println(LivingRoom.getRoomName());
 
         Player player1 = new Player();
         System.out.println("Как к вам обращаться?");
